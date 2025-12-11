071f2d5ef43d63215d3049145c469de9f0ad3cd2
LianisDevs <lianisdevs@gmail.com>
2025-12-11 14:05:52 +1100

    Added 12 Updated readme and header

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the node to count the number of leaves of
+ * Return: number of leaves or 0 on failure
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	/*cheching if node has 2 leaves*/
+	if (tree->left != NULL && tree->right != NULL)
+		return (2);
+
+	return (1);
+}
--- README.md
+++ README.md
@@ -23,3 +23,5 @@
 * 10- measures the depth of a node in a binary tree
 
 * 11- measures the size of a bianry tree
+
+* 12- counts the leaves in a binary tree
