7b10cbb34522b169ffd628e413fb9c3115a0f6b9
LianisDevs <lianisdevs@gmail.com>
2025-12-11 14:23:37 +1100

    Updated comments

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,7 +11,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	/*cheching if node has 2 leaves*/
+
+	/*base case*/
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
